5471e24dd63988dda5c809d31f2aae75451115a4
rudywaltz <david.barkoczi@gmail.com>
2017-05-25 23:43:40 +0200

    opening

--- home.php
+++ home.php
@@ -2,27 +2,34 @@
 <?php get_header('simple'); ?>
 <div class="op-content__leftside">
   <?php get_template_part( 'templates/nav', 'menu'); ?>
-  <?php $exhibitions = get_field('exhibition'); ?>
+  <?php $exhibitions = get_field('exhibitions'); ?>
   <?php if( $exhibitions ): ?>
     <ul class="op-exhibitionlist">
-      <?php while ( has_sub_field('exhibition') ) : ?>
-        <li class="op-exhibitionlist__details
-        <?php if( get_row_layout() == 'actual' ) echo 'op-exhibitionlist__details-actual' ?>
-        <?php if( get_row_layout() == 'future' ) echo 'op-exhibitionlist__details-next' ?>
-        op-exhibition">
+      <?php while ( has_sub_field('exhibitions') ) : ?>
+        <li class="op-exhibitionlist__details <?php if( get_row_layout() == 'actual' ) echo 'op-exhibitionlist__details-actual' ?> <?php if( get_row_layout() == 'future' ) echo 'op-exhibitionlist__details-next' ?> op-exhibition">
           <div class="op-exhibition__header">
             <?php
               $type = get_row_layout();
-            if ($type == 'previus' ):
-               pll_e( 'Volt' );
-            elseif($type == 'actual'):
-              pll_e( 'Most' );
-            elseif($type == 'future'):
-              pll_e( 'Majd' );
-            endif;
+              if(function_exists(pll_e)) {
+                if ($type == 'past' ):
+                  pll_e( 'Volt' );
+                elseif($type == 'now'):
+                  pll_e( 'Most' );
+                elseif($type == 'then'):
+                  pll_e( 'Majd' );
+                endif;
+              }
             ?>
           </div>
-          <div class="op-exhibition__title"><a href="<?php echo the_sub_field('link') ?>"><?php echo the_sub_field('title') ?></a></div>
+          <div class="op-exhibition__title">
+            <?php if(get_sub_field('link')): ?>
+              <a href="<?php echo the_sub_field('link') ?>">
+            <?php endif; ?>
+              <?php echo the_sub_field('title') ?>
+            <?php if(get_sub_field('link')): ?>
+              </a>
+            <?php endif; ?>
+          </div>
           <div class="op-exhibition__location"><?php echo the_sub_field('location') ?></div>
           <div class="op-exhibition__date"><?php echo the_sub_field('date_opening') ?> - <?php echo the_sub_field('date_end') ?></div>
         </li>
@@ -31,7 +38,7 @@
   <?php endif; ?>
 </div>
 <div class="op-content__rightside">
-<?php get_template_part('templates/slider', 'template'); ?>
+  <?php require_once('templates/slider.php'); ?>
 </div>
 <?php  get_footer(); ?>
 
