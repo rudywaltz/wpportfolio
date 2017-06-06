a46e073321e0326624c2ebea179f3237fa182b68
rudywaltz <david.barkoczi@gmail.com>
2017-06-06 13:13:14 +0200

    new logo

--- templates/nav-menu.php
+++ templates/nav-menu.php
@@ -1,5 +1,9 @@
 <header id="header" role="banner" class="op-header">
-    <div id="site-title"><img src="<?php echo get_template_directory_uri() .'/images/logo.jpg';?>" alt="" class="op-logo"></div>
+    <div id="site-title">
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home" class="op-header__homelink">
+      <img src="<?php echo get_template_directory_uri() .'/images/logo.svg';?>" alt="" class="op-logo">
+    </a>
+    </div>
   <nav id="menu" role="navigation" class="op-navigation">
     <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
   </nav>
